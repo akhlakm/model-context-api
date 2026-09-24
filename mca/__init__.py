5246b6ed52a8a25d06e78877642e7bcffca04558
akhlakm <me@akhlakm.com>
2026-09-24 14:20:12 -0400

    Bump version to 0.1.2

--- mca/__init__.py
+++ mca/__init__.py
@@ -3,6 +3,6 @@
 from .base import BaseMCARouter, GuideCatalog, MCAError
 from .composition import MCAClient
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = ["BaseMCARouter", "GuideCatalog", "MCAClient", "MCAError", "__version__"]
